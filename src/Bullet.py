582f8f97869154720166b48304711714602867bf
Zenjjim <zaim.imran@gmail.com>
2020-09-09 19:39:15 +0200

    update bullet

--- src/Bullet.py
+++ src/Bullet.py
@@ -2,15 +2,19 @@ import pygame
 import os
 
 
+IMAGE = os.path.join("assets", "carrot.png")
+BULLET_SCALE = (50, 40)
+VELOCITY = 5
+
 class Bullet(pygame.sprite.Sprite):
 
     def __init__(self):
         super().__init__()
 
-        self.image = pygame.image.load(os.path.join("assets", "carrot.png"))
-        self.image = pygame.transform.scale(self.image, (50, 40))
+        self.image = pygame.image.load(IMAGE)
+        self.image = pygame.transform.scale(self.image, BULLET_SCALE)
 
         self.rect = self.image.get_rect()
 
     def update(self):
-        self.rect.x += 5
+        self.rect.x += VELOCITY
